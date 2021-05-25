231b362c325598fd4a8cf6c5bc2e0aa02eaaa02c
suren <suren>
2021-05-25 15:06:41 +0100

    added kafka example

--- dotnet-examples/shop/Services/OrdersService.cs
+++ dotnet-examples/shop/Services/OrdersService.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Confluent.Kafka;
 
 namespace Shop.Services
 {
+    public enum OrderType
+    {
+        Default,
+        DiscountSale,
+        BulkOrder
+    }
+
     public class OrdersService
     {
         public HttpClient Client { get; }
+
+        private readonly string _topic = "orders";
+        private readonly string _brokers = "localhost:9093";
 
         public OrdersService(HttpClient client)
         {
@@ -17,6 +28,66 @@ namespace Shop.Services
         public async Task<string> GetSomeText(string base64Str)
         {
             return await Client.GetStringAsync($"base64/{base64Str}");
+        }
+
+        public async Task PlaceOrderAsync(OrderType orderType)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _brokers
+            };
+
+            // Note: by default strings are encoded as UTF8.  (JSON, Avro are supported too)
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var msg = new Message<Null, string> { Value = "Order Placed" };
+
+                    // asynchronous wait on delivery report 
+                    var dr = await p.ProduceAsync(_topic, msg);
+
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+
+        // Run it in a separate Task to stop it from blocking the main thread.
+        public void PlaceBulkOrders(int count)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _brokers
+            };
+
+            Action<DeliveryReport<string, string>> handler = r =>
+            {
+                if (r.Error.IsError)
+                {
+                    Console.WriteLine($"Delivery Error: {r.Error.Reason}");
+                }
+                // send it to another topic or service if needed.
+                Console.WriteLine($"Message delivered to {r.TopicPartitionOffset}");
+            };
+
+            using (var p = new ProducerBuilder<string, string>(config).Build())
+            {
+                for (int i = 1; i <= count; ++i)
+                {
+                    var msg = new Message<string, string>
+                    {
+                        Key = OrderType.BulkOrder.ToString(),
+                        Value = $"BulkOrder-{i}"
+                    };
+                    p.Produce(_topic, msg, handler);
+                }
+                // wait for up to 5 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(5));
+            }
         }
     }
 }
--- dotnet-examples/shop/pages/index.cshtml.cs
+++ dotnet-examples/shop/pages/index.cshtml.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using Shop.Services;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shop.Pages
@@ -11,7 +8,7 @@ namespace Shop.Pages
     {
         private readonly OrdersService _ordersService;
 
-        public string Message { get; private set; } = "Welcome to the Shop.";
+        public string Message { get; private set; } = "Welcome to Demo Shop.";
 
         public string OrderStatus { get; set; }
 
@@ -23,7 +20,7 @@ namespace Shop.Pages
 
         public async Task OnGetAsync()
         {
-            Message = await _ordersService.GetSomeText("aGVsbG8gd29ybGQK");
+            //Message = await _ordersService.GetSomeText("aGVsbG8gd29ybGQK");            
         }
 
         public async Task OnPostAsync()
